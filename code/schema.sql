8eb6bcfcda9b43c4d2332c408e66622ee93d2d77
JackYannes56 <42744976+JackYannes56@users.noreply.github.com>
2020-05-03 12:19:44 -0400

    added schema for Employment and Population tables

--- code/schema.sql
+++ code/schema.sql
@@ -1,4 +1,24 @@
-CREATE TABLE test(
-    a VARCHAR(15),
-    b INT
+CREATE TABLE IF NOT EXISTS Employment(
+    areaType VARCHAR(127),
+    area VARCHAR(127),
+    NAICS INT,
+    NAICSTitle VARCHAR(127),
+    year INT,
+    Establishments INT,
+    averageEmployment INT,
+    totalWage BIGINT,
+    annualAverageSalary BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS Population(
+	year INT,
+	ageGroupCode INT,
+	ageGroupDescription VARCHAR(127),
+	genderCode INT,
+	genderDescription VARCHAR(127),
+	raceCode INT,
+	raceDescription VARCHAR(127),
+	countyCode INT,
+	countyName VARCHAR(127),
+	population INT
 );
