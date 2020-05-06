93231b39003f8dd23dca390f8d72a753aab78985
123joshuawu <joshuawu3@gmail.com>
2020-05-05 22:58:34 -0700

    Add application

--- code/database.py
+++ code/database.py
@@ -7,22 +7,22 @@ class Queries:
     def __init__(self, connection_string):
         self.conn = psycopg2.connect(connection_string)
 
-    def populationChange(self, county, year1, year2):
-        #this query takes a county and 2 years and return the difference in population between the 2 years. Make sure to check that the users are entering valid years and county names.
+    def populationChange(self, county_name, start_year, end_year):
+        #this query takes a county_name and 2 years and return the difference in population between the 2 years. Make sure to check that the users are entering valid years and county_name names.
         cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         query = "SELECT Population.population - pop2.population from \
             Population inner join Population as pop2 on Population.countyName = pop2.countyName where upper(Population.countyName) \
                 like upper(%s) and Population.year = %s and pop2.year = %s and Population.ageGroupCode = %s and pop2.ageGroupCode = %s \
                     and Population.genderCode = %s and pop2.genderCode = %s and Population.raceCode = %s and pop2.raceCode = %s"
-        cursor.execute(query, (county, year1, year2, 0, 0, 0, 0, 0, 0,))
+        cursor.execute(query, (county_name, start_year, end_year, 0, 0, 0, 0, 0, 0,))
         records = cursor.fetchone()
         return records
 
-    def employmentChange(self, county, year1, year2):
-        #this query takes a county and 2 years and returns the difference in avgEmployment between the 2 years. Make sure to check that users are entering a valid county name and valid years.
+    def employmentChange(self, county_name, start_year, end_year):
+        #this query takes a county_name and 2 years and returns the difference in avgEmployment between the 2 years. Make sure to check that users are entering a valid county_name name and valid years.
         cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        county = county.strip() + " County"
-        cursor.execute("SELECT Employment.averageEmployment - emp2.averageEmployment from (Employment join Employment as emp2 on Employment.area = emp2.area and upper(Employment.area) like upper(%s) and Employment.year = %s and emp2.year = %s and Employment.NAICS = %s and emp2.NAICS = %s)", (county, year1, year2, 0, 0,))
+        county_name = county_name.strip() + " County"
+        cursor.execute("SELECT Employment.averageEmployment - emp2.averageEmployment from (Employment join Employment as emp2 on Employment.area = emp2.area and upper(Employment.area) like upper(%s) and Employment.year = %s and emp2.year = %s and Employment.NAICS = %s and emp2.NAICS = %s)", (county_name, start_year, end_year, 0, 0,))
         records = cursor.fetchone()
         return records
 
@@ -42,17 +42,17 @@ class Queries:
         records = cursor.fetchone()
         return records
     
-    def getPopulationByCounty(self, county):
+    def getPopulationByCounty(self, county_name):
         cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         query = "SELECT * FROM Population where upper(countyName) like upper(%s)"
-        cursor.execute(query, (county,))
+        cursor.execute(query, (county_name,))
         records = cursor.fetchall()
         return records
 
-    def getEmploymentByCounty(self, county):
+    def getEmploymentByCounty(self, county_name):
         cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        county = county.strip() + " County"
+        county_name = county_name.strip() + " County"
         query = "SELECT * FROM Employment where upper(area) like upper(%s)"
-        cursor.execute(query, (county,))
+        cursor.execute(query, (county_name,))
         records = cursor.fetchall()
         return records
